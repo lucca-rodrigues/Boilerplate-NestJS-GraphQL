382ad4e38a11d3867e9100ca28c9e828ba66ffed
Lucas Rodrigues <contato.luccarodrigues@gmail.com>
2021-12-17 14:04:34 -0300

    add graphqal configs on crud

--- src/likes/likes.service.ts
+++ src/likes/likes.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateLikeInput } from './dto/create-like.input';
 import { UpdateLikeInput } from './dto/update-like.input';
+import { Like } from './entities/like.entity';
 
 @Injectable()
 export class LikesService {
@@ -8,19 +9,19 @@ export class LikesService {
     return { exampleField: createLikeInput.exampleField };
   }
 
-  findAll() {
-    return { exampleField: 1 };
+  findAll(): [Like] {
+    return [{ exampleField: 1 }];
   }
 
-  findOne(id: number) {
+  findOne(id: number): Like {
     return { exampleField: id };
   }
 
-  update(id: number, updateLikeInput: UpdateLikeInput) {
+  update(id: number, updateLikeInput: UpdateLikeInput): Like {
     return { exampleField: updateLikeInput.id };
   }
 
-  remove(id: number) {
+  remove(id: number): Like {
     return { exampleField: id };
   }
 }

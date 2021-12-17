47ac78f31fd56fd743362fd39fadaf45b4e6d3e0
Lucas Rodrigues <contato.luccarodrigues@gmail.com>
2021-12-17 15:59:15 -0300

    create configs typeorm graphQL and postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,34 +1,35 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
-import { UserModule } from './user/user.module';
-import { PostsModule } from './posts/posts.module';
-import { User } from './user/entities/user.entity';
-import { Post } from './posts/entities/post.entity';
+import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { LikesModule } from './likes/likes.module';
+import * as dotenv from 'dotenv';
+import { User } from './users/entities/user.entity';
+dotenv.config();
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
+      autoSchemaFile: './schema.gql',
       debug: true,
       playground: true,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
-    // TypeOrmModule.forRoot({
-    //   name: 'boilerplate',
-    //   type: 'postgres',
-    //   host: 'localhost',
-    //   port: 5432,
-    //   username: 'boilerplate',
-    //   password: 'boilerplate',
-    //   database: 'boilerplate',
-    //   entities: [User, Post],
-    //   //logging: true,
-    // }),
-    UserModule,
-    PostsModule,
-    LikesModule,
+    TypeOrmModule.forRoot({
+      // keepConnectionAlive: true,
+      // name: 'boilerplate',
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'boilerplate',
+      password: 'boilerplate',
+      database: 'boilerplate',
+      autoLoadEntities: true,
+      // synchronize: true,
+      // entities: [User],
+      //logging: true,
+    }),
+    UsersModule,
   ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/user/entities/user.entity.ts
+++
@@ -1,9 +0,0 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
-import { PrimaryGeneratedColumn } from 'typeorm';
-
-@ObjectType()
-export class User {
-  @PrimaryGeneratedColumn('uuid')
-  @Field(() => Int)
-  id: number;
-}
